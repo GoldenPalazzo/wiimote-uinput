125b936d9979611ed578fb29b248dc76aee98573
Francesco Palazzo <francesco@palazzo.me>
2026-01-04 13:20:15 +0100

    Proper prototype

--- src/spoofer.h
+++ src/spoofer.h
@@ -3,6 +3,6 @@
 #include "wiimote.h"
 
 void wiimote_to_uinput(const wiimote_state_t *wiimote, int uinput_fd);
-int create_uinput_device();
+int create_uinput_device(void);
 int destroy_uinput_device(int fd);
 #endif
